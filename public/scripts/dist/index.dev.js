784a758b5c14c2209e2a670e731e4c43028420ec
Ayomide <sonugaengineer@gmail.com>
2020-09-07 10:45:43 +0100

    v2

--- public/scripts/dist/index.dev.js
+++ public/scripts/dist/index.dev.js
@@ -9,7 +9,88 @@ var p = document.getElementsByTagName('p');
 var city = document.querySelector('.city');
 var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
-var information = document.querySelector('.information');
+var locationData = document.getElementById('locationData');
+var moreInfo = document.getElementById('moreInfo');
+var h1 = document.querySelector('.h1');
+var myLocation = document.getElementById('myLocation');
+var longitude;
+var latitude;
+var isCacheSupported = 'caches' in window;
+var cacheName = 'weatherForecast'; //gets the current date and time
+
+function getDate() {
+  var today = new Date();
+  time = timeConverter(today.getTime());
+  h1.innerHTML += today;
+}
+
+getDate(); //function to convert time from UNIX time to EPOCH time
+
+function timeConverter(UNIX_timestamp) {
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+  return time;
+}
+
+function timeConverter1(UNIX_timestamp) {
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var time = date + ' ' + month + ' ' + year;
+  return time;
+}
+
+var current = document.querySelector('.current');
+
+function initCoords() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(currentLocation);
+  } else {//
+  }
+}
+
+initCoords();
+
+function currentLocation(position) {
+  longitude = position.coords.longitude;
+  latitude = position.coords.latitude;
+  var currentLocationData = "https://api.openweathermap.org/data/2.5/weather?lat=".concat(latitude, "&lon=").concat(longitude, "&appid=").concat(apiKey, "&units=metric");
+  fetch(currentLocationData).then(function (Response) {
+    return Response.json();
+  }).then(function (data) {
+    var main = data.main,
+        name = data.name,
+        sys = data.sys,
+        weather = data.weather;
+    var icon = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/".concat(weather[0]["icon"], ".svg");
+    var li = document.createElement("li");
+    var markup = "\n          <h2 class=\"location-name\" data-name = ".concat(name, ",").concat(sys.country, "\">\n            <span>").concat(name, "</span>,\n            <span>").concat(sys.country, "</span>\n          </h2>\n          <div class=\"location-temp\">").concat(Math.round(main.temp), "<sup>\xB0C</sup></div>\n          <figure>\n            <img class=\"location-icon\" src=\"").concat(icon, "\" alt=\"").concat(weather[0]["description"], "\">\n            <figcaption>").concat(weather[0]["description"], "</figcaption>\n          </figure>\n        ");
+    li.innerHTML = markup;
+    locationData.appendChild(li);
+  });
+  fetch(currentLocationData).then(function (Response) {
+    return Response.json();
+  }).then(function (data) {
+    var main = data.main,
+        sys = data.sys,
+        wind = data.wind,
+        clouds = data.clouds;
+    var li = document.createElement("li");
+    var markup = "\n      <div class=\"feelsLike\">Feels Like ".concat(Math.round(main.feels_like), "<sup>\xB0C</sup></div>\n      <div class=\"humidity\">Humidity: ").concat(main.humidity, "%</div>\n      <div class=\"windSpeed\">Wind Speed: ").concat(wind.speed, "m/s</div>\n      <div class=\"windDirection\">Wind Direction: ").concat(wind.deg, "<sup>\xB0</sup></div>\n      <div class=\"cloudiness\">Cloudiness: ").concat(clouds.all, "%</div>\n      <div class=\"pressure\">Atmospheric pressure : ").concat(main.pressure, "hPa</div>\n      <div class=\"sunrise\">Sunrise: ").concat(timeConverter(sys.sunrise), "am</div>\n      <div class=\"sunset\">Sunset: ").concat(timeConverter(sys.sunset), "pm</div>\n    ");
+    li.innerHTML = markup;
+    moreInfo.appendChild(li);
+  });
+}
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   var inputVal = input.value; //check if there's already a city
@@ -61,81 +142,17 @@ form.addEventListener("submit", function (e) {
     cities.appendChild(li);
   })["catch"](function () {
     msg.textContent = "Please search for a valid city";
-  }); //function to convert time from UNIX time to EPOCH time
-
-  function timeConverter(UNIX_timestamp) {
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
-    return time;
-  }
-
-  function openModal() {
-    fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (data) {
-      var main = data.main,
-          name = data.name,
-          sys = data.sys,
-          coord = data.coord,
-          wind = data.wind,
-          clouds = data.clouds,
-          weather = data.weather;
-      var icon = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/".concat(weather[0]["icon"], ".svg");
-      var li = document.createElement("li");
-      li.classList.add("info");
-      var markup = "\n        <div class=\"more-info-temp\">Feels Like: ".concat(Math.round(main.feels_like), "<sup>\xB0C</sup></div>\n        <div class=\"more-info\">Minimum temperature: ").concat(Math.round(main.temp_min), "<sup>\xB0C</sup></div>\n        <div class=\"more-info\">Maximum temperature : ").concat(Math.round(main.temp_max), "<sup>\xB0C</sup></div>\n        <div class=\"more-info\">Humidity: ").concat(main.humidity, "%</div>\n        <div class=\"more-info\">Wind Speed: ").concat(wind.speed, "m/s</div>\n        <div class=\"more-info\">Wind Direction: ").concat(wind.deg, "<sup>\xB0</sup></div>\n        <div class=\"more-info\">Cloudiness: ").concat(clouds.all, "%</div>\n        <div class=\"more-info\">Atmospheric pressure : ").concat(main.pressure, "hPa</div>\n        <div class=\"more-info\">Longitude: ").concat(coord.lon, "</div>\n        <div class=\"more-info\">Latitude: ").concat(coord.lat, "</div>\n        <div class=\"more-info\">Sunrise: ").concat(timeConverter(sys.sunrise), "am</div>\n        <div class=\"more-info\">Sunset: ").concat(timeConverter(sys.sunset), "pm</div>\n      ");
-      li.innerHTML = markup;
-      information.appendChild(li);
-      var li1 = document.createElement("li");
-      li1.classList.add("city");
-      var markup1 = "\n        <h2 class=\"city-name\" data-name=\"".concat(name, ",").concat(sys.country, "\">\n          <span>").concat(name, "</span>\n          <sup>").concat(sys.country, "</sup>\n        </h2>\n        <div class=\"city-temp\">").concat(Math.round(main.temp), "<sup>\xB0C</sup></div>\n        <figure>\n          <img class=\"city-icon\" src=\"").concat(icon, "\" alt=\"").concat(weather[0]["description"], "\">\n          <figcaption>").concat(weather[0]["description"], "</figcaption>\n        </figure>\n      ");
-      li1.innerHTML = markup1;
-      information.appendChild(li1);
-    });
-  } //delete weather result from site
-
+  }); //delete weather result from site
 
   for (var i = 0; i < p.length; i++) {
     // loop over them
     p[i].addEventListener('click', function (e) {
       console.log(i);
       var city = document.querySelector('.city');
-      modal.style.display = "block";
-      openModal(); //city.remove();
+      modal.style.display = "block"; //city.remove();
       //localStorage.removeItem(i);
     });
-  } // When the user clicks on the weather box, open the modal
-
-
-  if (city) {
-    var city = document.querySelector('.city');
-
-    for (var _i = 0; _i < city.length; _i++) {
-      city[_i].addEventListener('click', function (e) {
-        modal.style.display = "block"; //openModal();
-      });
-    }
-  } // When the user clicks on <span> (x), close the modal
-
-
-  span.onclick = function () {
-    modal.style.display = "none";
-    information.innerHTML = '';
-  };
-
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-      information.innerHTML = '';
-    }
-  }; //localstorage function
+  } //localstorage function
 
 
   function saveLocationList(add_item) {
@@ -147,25 +164,49 @@ form.addEventListener("submit", function (e) {
       localStorage.setItem("list_items", JSON.stringify(existingEntries));
     } else {
       // or tell user it's already there
-      console.log(add_item + ' already exists'); //
-    }
-  } //Loads the list of saved location.
-
-
-  function loadLocationList() {
-    for (var i = 0; i < localStorage.length; i++) {
-      //console.log(localStorage.getItem(localStorage.key(i)));
-      var key = localStorage.key(i);
-      var weatherdata = JSON.parse(localStorage.getItem(key));
-      var li = document.createElement("li");
-      li.classList.add("city");
-      li.innerHTML = weatherdata.split(",");
-      cities.appendChild(li);
+      console.log(add_item + ' already exists');
     }
   }
 
-  loadLocationList();
   msg.textContent = "";
   form.reset();
   input.focus();
-});
+
+  if (!('caches' in window)) {
+    return null;
+  }
+
+  return caches.match(url).then(function (response) {
+    if (response) {
+      return response.json();
+    }
+
+    return null;
+  })["catch"](function (err) {
+    console.error('Error getting data from cache', err);
+    return null;
+  });
+}); //Loads the list of saved location.
+
+window.onload = function () {
+  var _loop = function _loop() {
+    var key = localStorage.key(i);
+    var weatherdata = JSON.parse(localStorage.getItem(key));
+
+    if (weatherdata !== null) {
+      weatherdata.forEach(function () {
+        var li = document.createElement("li");
+        li.classList.add("city");
+        li.innerHTML += weatherdata;
+        cities.appendChild(li);
+      });
+    } else {
+      //if nothing exist in storage, keep array empty
+      weatherdata = [];
+    }
+  };
+
+  for (var i = 0; i < localStorage.length; i++) {
+    _loop();
+  }
+};
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -7,7 +7,113 @@ let p = document.getElementsByTagName('p');
 let city = document.querySelector('.city');
 let modal = document.getElementById("myModal");
 let span = document.getElementsByClassName("close")[0];
-let information = document.querySelector('.information');
+let locationData = document.getElementById('locationData');
+let moreInfo = document.getElementById('moreInfo');
+let h1 = document.querySelector('.h1');
+let myLocation = document.getElementById('myLocation');
+let longitude;
+let latitude;
+let isCacheSupported = 'caches' in window;
+let cacheName = 'weatherForecast'; 
+
+
+//gets the current date and time
+function getDate() {
+  var today = new Date();
+  time = timeConverter(today.getTime());
+  h1.innerHTML += today;
+}
+getDate();
+
+//function to convert time from UNIX time to EPOCH time
+function timeConverter(UNIX_timestamp) {
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+  return time;
+}
+
+function timeConverter1(UNIX_timestamp) {
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var time = date + ' ' + month + ' ' + year;
+  return time;
+}
+
+let current = document.querySelector('.current');
+function initCoords() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(currentLocation);
+  } else {
+    //
+  }
+}
+initCoords();
+
+function currentLocation(position) {
+  longitude = position.coords.longitude;
+  latitude = position.coords.latitude;
+
+  const currentLocationData = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  fetch(currentLocationData)
+    .then(Response => Response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0]["icon"]
+        }.svg`;
+
+      const li = document.createElement("li");
+      const markup = `
+          <h2 class="location-name" data-name = ${name},${sys.country}">
+            <span>${name}</span>,
+            <span>${sys.country}</span>
+          </h2>
+          <div class="location-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+          <figure>
+            <img class="location-icon" src="${icon}" alt="${
+        weather[0]["description"]
+        }">
+            <figcaption>${weather[0]["description"]}</figcaption>
+          </figure>
+        `;
+      li.innerHTML = markup;
+      locationData.appendChild(li);
+    })
+
+    fetch(currentLocationData)
+    .then(Response => Response.json())
+    .then(data => {
+      const { main, sys, wind, clouds, } = data;
+
+      const li = document.createElement("li");
+      const markup = `
+      <div class="feelsLike">Feels Like ${Math.round(main.feels_like)}<sup>°C</sup></div>
+      <div class="humidity">Humidity: ${(main.humidity)}%</div>
+      <div class="windSpeed">Wind Speed: ${(wind.speed)}m/s</div>
+      <div class="windDirection">Wind Direction: ${(wind.deg)}<sup>°</sup></div>
+      <div class="cloudiness">Cloudiness: ${(clouds.all)}%</div>
+      <div class="pressure">Atmospheric pressure : ${(main.pressure)}hPa</div>
+      <div class="sunrise">Sunrise: ${timeConverter(sys.sunrise)}am</div>
+      <div class="sunset">Sunset: ${timeConverter(sys.sunset)}pm</div>
+    `;
+      li.innerHTML = markup;
+      moreInfo.appendChild(li);
+    })
+
+}
 
 form.addEventListener("submit", e => {
   e.preventDefault();
@@ -59,7 +165,6 @@ form.addEventListener("submit", e => {
         weather[0]["icon"]
         }@2x.png`;
 
-
       const li = document.createElement("li");
       li.classList.add("city");
       const markup = `
@@ -86,102 +191,15 @@ form.addEventListener("submit", e => {
       msg.textContent = "Please search for a valid city";
     });
 
-  //function to convert time from UNIX time to EPOCH time
-  function timeConverter(UNIX_timestamp) {
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
-    return time;
-  }
-
-  function openModal() {
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const { main, name, sys, coord, wind, clouds, weather } = data;
-        const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-          weather[0]["icon"]
-          }.svg`;
-
-        const li = document.createElement("li");
-        li.classList.add("info");
-        const markup = `
-        <div class="more-info-temp">Feels Like: ${Math.round(main.feels_like)}<sup>°C</sup></div>
-        <div class="more-info">Minimum temperature: ${Math.round(main.temp_min)}<sup>°C</sup></div>
-        <div class="more-info">Maximum temperature : ${Math.round(main.temp_max)}<sup>°C</sup></div>
-        <div class="more-info">Humidity: ${(main.humidity)}%</div>
-        <div class="more-info">Wind Speed: ${(wind.speed)}m/s</div>
-        <div class="more-info">Wind Direction: ${(wind.deg)}<sup>°</sup></div>
-        <div class="more-info">Cloudiness: ${(clouds.all)}%</div>
-        <div class="more-info">Atmospheric pressure : ${(main.pressure)}hPa</div>
-        <div class="more-info">Longitude: ${coord.lon}</div>
-        <div class="more-info">Latitude: ${coord.lat}</div>
-        <div class="more-info">Sunrise: ${timeConverter(sys.sunrise)}am</div>
-        <div class="more-info">Sunset: ${timeConverter(sys.sunset)}pm</div>
-      `;
-        li.innerHTML = markup;
-        information.appendChild(li);
-
-        const li1 = document.createElement("li");
-        li1.classList.add("city");
-        const markup1 = `
-        <h2 class="city-name" data-name="${name},${sys.country}">
-          <span>${name}</span>
-          <sup>${sys.country}</sup>
-        </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-        <figure>
-          <img class="city-icon" src="${icon}" alt="${
-          weather[0]["description"]
-          }">
-          <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>
-      `;
-        li1.innerHTML = markup1;
-        information.appendChild(li1);
-      })
-  }
-
   //delete weather result from site
   for (var i = 0; i < p.length; i++) { // loop over them
     p[i].addEventListener('click', function (e) {
       console.log(i);
       var city = document.querySelector('.city');
       modal.style.display = "block";
-      openModal();
       //city.remove();
       //localStorage.removeItem(i);
     });
-  }
-
-  // When the user clicks on the weather box, open the modal
-  if (city) {
-    var city = document.querySelector('.city');
-    for (let i = 0; i < city.length; i++) {
-      city[i].addEventListener('click', function (e) {
-        modal.style.display = "block";
-        //openModal();
-      })
-    }
-  }
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
-    modal.style.display = "none";
-    information.innerHTML = '';
-  }
-
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-      information.innerHTML = '';
-    }
   }
 
   //localstorage function
@@ -196,25 +214,45 @@ form.addEventListener("submit", e => {
     } else {
       // or tell user it's already there
       console.log(add_item + ' already exists')
-      //
     }
   }
-
-  //Loads the list of saved location.
-  function loadLocationList() {
-    for (var i = 0; i < localStorage.length; i++) {
-      //console.log(localStorage.getItem(localStorage.key(i)));
-      let key = localStorage.key(i);
-      let weatherdata = JSON.parse(localStorage.getItem(key));
-      const li = document.createElement("li");
-      li.classList.add("city");
-      li.innerHTML = weatherdata.split(",");   
-      cities.appendChild(li);
-    }
-  }
-  loadLocationList();
 
   msg.textContent = "";
   form.reset();
   input.focus();
+
+  if (!('caches' in window)) {
+    return null;
+  }
+
+  return caches.match(url)
+    .then((response) => {
+      if (response) {
+        return response.json();
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.error('Error getting data from cache', err);
+      return null;
+    });
 });
+
+//Loads the list of saved location.
+window.onload = function () {
+  for (var i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let weatherdata = JSON.parse(localStorage.getItem(key));
+    if (weatherdata !== null) {
+      weatherdata.forEach(function () {
+        const li = document.createElement("li");
+        li.classList.add("city");
+        li.innerHTML += weatherdata;
+        cities.appendChild(li);
+      })
+    } else { //if nothing exist in storage, keep array empty
+      weatherdata = [];
+    }
+  }
+}
+
