47559dcabaed6c4b95ca0e20a1d008b76bc238bb
Ayomide <sonugaengineer@gmail.com>
2020-09-10 12:08:07 +0100

    v3

--- scripts/dist/index.dev.js
+++ scripts/dist/index.dev.js
@@ -7,16 +7,12 @@ var cities = document.querySelector(".results .cities");
 var apiKey = "d9d1610fdb5a0a14f64e9add8173e17a";
 var p = document.getElementsByTagName('p');
 var city = document.querySelector('.city');
-var modal = document.getElementById("myModal");
-var span = document.getElementsByClassName("close")[0];
 var locationData = document.getElementById('locationData');
 var moreInfo = document.getElementById('moreInfo');
 var h1 = document.querySelector('.h1');
-var myLocation = document.getElementById('myLocation');
 var longitude;
 var latitude;
-var isCacheSupported = 'caches' in window;
-var cacheName = 'weatherForecast'; //gets the current date and time
+var windDirection = document.querySelector('.windDirection'); //gets the current date and time
 
 function getDate() {
   var today = new Date();
@@ -24,7 +20,44 @@ function getDate() {
   h1.innerHTML += today;
 }
 
-getDate(); //function to convert time from UNIX time to EPOCH time
+getDate();
+
+function getWindDirection(number) {
+  if (number > 348.75 && number < 11.25) {
+    windDirection.innerHTML += "N";
+  } else if (number > 11.25 && number < 33.75) {
+    windDirection.innerHTML += "NNE";
+  } else if (number > 33.75 && number < 56.25) {
+    windDirection.innerHTML += "NE";
+  } else if (number > 56.25 && number < 78.75) {
+    windDirection.innerHTML += "ENE";
+  } else if (number > 78.75 && number < 101.25) {
+    windDirection.innerHTML += "E";
+  } else if (number > 101.25 && number < 123.75) {
+    windDirection.innerHTML += "ESE";
+  } else if (number > 123.75 && number < 146.25) {
+    windDirection.innerHTML += "SE";
+  } else if (number > 146.25 && number < 168.75) {
+    windDirection.innerHTML += "SSE";
+  } else if (number > 168.75 && number < 191.25) {
+    windDirection.innerHTML += "S";
+  } else if (number > 191.25 && number < 213.75) {
+    windDirection.innerHTML += "SSW";
+  } else if (number > 213.75 && number < 236.25) {
+    windDirection.innerHTML += "SW";
+  } else if (number > 236.25 && number < 258.75) {
+    windDirection.innerHTML += "WSW";
+  } else if (number > 258.75 && number < 281.25) {
+    windDirection.innerHTML += "W";
+  } else if (number > 281.25 && number < 303.75) {
+    windDirection.innerHTML += "WNW";
+  } else if (number > 303.75 && number < 326.25) {
+    windDirection.innerHTML += "NW";
+  } else if (number > 326.25 && number < 348.75) {
+    windDirection.innerHTML += "NNW";
+  }
+} //function to convert time from UNIX time to EPOCH time
+
 
 function timeConverter(UNIX_timestamp) {
   var a = new Date(UNIX_timestamp * 1000);
@@ -54,7 +87,7 @@ var current = document.querySelector('.current');
 function initCoords() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(currentLocation);
-  } else {//
+  } else {//.innerHTML = "";
   }
 }
 
@@ -73,7 +106,7 @@ function currentLocation(position) {
         weather = data.weather;
     var icon = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/".concat(weather[0]["icon"], ".svg");
     var li = document.createElement("li");
-    var markup = "\n          <h2 class=\"location-name\" data-name = ".concat(name, ",").concat(sys.country, "\">\n            <span>").concat(name, "</span>,\n            <span>").concat(sys.country, "</span>\n          </h2>\n          <div class=\"location-temp\">").concat(Math.round(main.temp), "<sup>\xB0C</sup></div>\n          <figure>\n            <img class=\"location-icon\" src=\"").concat(icon, "\" alt=\"").concat(weather[0]["description"], "\">\n            <figcaption>").concat(weather[0]["description"], "</figcaption>\n          </figure>\n        ");
+    var markup = "\n          <h2 class=\"location-name\" data-name = ".concat(name, ",").concat(sys.country, "\">\n            <span>").concat(name, "</span>,\n            <span>").concat(sys.country, "</span>\n          </h2>\n          <div class=\"location-temp\">").concat(Math.round(main.temp), "<sup>\xB0C</sup></div>\n          <figure>\n            <img class=\"location-icon\" src=\"").concat(icon, "\" alt=\"").concat(weather[0]["description"], "\">\n            <figcaption>").concat(weather[0]["description"], "</figcaption>\n          </figure>");
     li.innerHTML = markup;
     locationData.appendChild(li);
   });
@@ -85,7 +118,7 @@ function currentLocation(position) {
         wind = data.wind,
         clouds = data.clouds;
     var li = document.createElement("li");
-    var markup = "\n      <div class=\"feelsLike\">Feels Like ".concat(Math.round(main.feels_like), "<sup>\xB0C</sup></div>\n      <div class=\"humidity\">Humidity: ").concat(main.humidity, "%</div>\n      <div class=\"windSpeed\">Wind Speed: ").concat(wind.speed, "m/s</div>\n      <div class=\"windDirection\">Wind Direction: ").concat(wind.deg, "<sup>\xB0</sup></div>\n      <div class=\"cloudiness\">Cloudiness: ").concat(clouds.all, "%</div>\n      <div class=\"pressure\">Atmospheric pressure : ").concat(main.pressure, "hPa</div>\n      <div class=\"sunrise\">Sunrise: ").concat(timeConverter(sys.sunrise), "am</div>\n      <div class=\"sunset\">Sunset: ").concat(timeConverter(sys.sunset), "pm</div>\n    ");
+    var markup = "\n      <div class=\"feelsLike\">Feels Like ".concat(Math.round(main.feels_like), "<sup>\xB0C</sup></div>\n      <div class=\"humidity\">Humidity: ").concat(main.humidity, "%</div>\n      <div class=\"windSpeed\">Wind Speed: ").concat(wind.speed, "m/s</div>\n      <div class=\"windDirection\">Wind Direction: ").concat(getWindDirection(wind.deg), "<sup>\xB0</sup></div>\n      <div class=\"cloudiness\">Cloudiness: ").concat(clouds.all, "%</div>\n      <div class=\"pressure\">Atmospheric pressure : ").concat(main.pressure, "hPa</div>\n      <div class=\"sunrise\">Sunrise: ").concat(timeConverter(sys.sunrise), "am</div>\n      <div class=\"sunset\">Sunset: ").concat(timeConverter(sys.sunset), "pm</div>\n    ");
     li.innerHTML = markup;
     moreInfo.appendChild(li);
   });
@@ -144,16 +177,18 @@ form.addEventListener("submit", function (e) {
     msg.textContent = "Please search for a valid city";
   }); //delete weather result from site
 
-  for (var i = 0; i < p.length; i++) {
-    // loop over them
-    p[i].addEventListener('click', function (e) {
-      console.log(i);
-      var city = document.querySelector('.city');
-      modal.style.display = "block"; //city.remove();
-      //localStorage.removeItem(i);
-    });
-  } //localstorage function
+  function removeWeatherResult() {
+    for (var i = 0; i < p.length; i++) {
+      // loop over them
+      p[i].addEventListener('click', function (e) {
+        console.log(i); //var city = document.querySelector('.city');
 
+        city.remove(); //localStorage.removeItem(i);
+      });
+    }
+  }
+
+  removeWeatherResult(); //localstorage function
 
   function saveLocationList(add_item) {
     // parse existing storage key or string representation of empty array
@@ -197,8 +232,10 @@ window.onload = function () {
       weatherdata.forEach(function () {
         var li = document.createElement("li");
         li.classList.add("city");
-        li.innerHTML += weatherdata;
-        cities.appendChild(li);
+        cities.innerHTML = "";
+        li.innerHTML = weatherdata;
+        li.style.display = 'grid';
+        cities.appendChild(li); //removeWeatherResult();
       });
     } else {
       //if nothing exist in storage, keep array empty
