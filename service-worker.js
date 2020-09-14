3c228cfae338f79293f4d335134c1d60e143cd40
Ayomide <sonugaengineer@gmail.com>
2020-09-14 17:46:17 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_NAME = 'static-cache-v8';
-const DATA_CACHE_NAME = 'data-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v3';
 
 //list of files to cache.
 const FILES_TO_CACHE = [
